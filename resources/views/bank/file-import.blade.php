f7ab9c58b856bb6fa70bb4e813964aec2a60edd4
shebas72 <shebas.veer@gmail.com>
2021-10-10 11:04:41 +0300

    sec

--- resources/views/bank/file-import.blade.php
+++ resources/views/bank/file-import.blade.php
@@ -77,12 +77,24 @@
                             <td>{{ $bank->Condition }}</td>
                             <td>{{ $bank->Key_Available }}</td>
                            
-                            <td><a href="" class="delete-row"><i class="fas fa-pencil-alt"></i></a>&nbsp;<a href="" class="delete-row"><i class="far fa-trash-alt"></i></a></td>
+                            <td><a href="" id="editCompany" data-toggle="modal" data-target='#practice_modal' data-id="{{ $bank->id }}"><i class="fas fa-pencil-alt"></i></a>&nbsp;<a href="" class="delete-row"><i class="far fa-trash-alt"></i></a></td>
                         </tr>
                         @endforeach   
                         
                             
-                            
+                        <div class="modal fade" id="practice_modal">
+                            <div class="modal-dialog">
+                               <form id="companydata">
+                                    <div class="modal-content">
+                                    <input type="hidden" id="color_id" name="color_id" value="">
+                                    <div class="modal-body">
+                                        <input type="text" name="name" id="name" value="" class="form-control">
+                                    </div>
+                                    <input type="submit" value="Submit" id="submit" class="btn btn-sm btn-outline-danger py-0" style="font-size: 0.8em;">
+                                </div>
+                               </form>
+                            </div>
+                        </div>
                         
                         </tbody>
                     </table>    
@@ -94,5 +106,58 @@
 
         </div>
     </div>
+
+    @push('script')
+{{-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>   --}}
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
+<script>
+
+$(document).ready(function () {
+
+$.ajaxSetup({
+    headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+});
+
+$('body').on('click', '#submit', function (event) {
+    event.preventDefault()
+    var id = $("#color_id").val();
+    var name = $("#name").val();
+   
+    $.ajax({
+      url: 'color/' + id,
+      type: "POST",
+      data: {
+        id: id,
+        name: name,
+      },
+      dataType: 'json',
+      success: function (data) {
+          
+          $('#companydata').trigger("reset");
+          $('#practice_modal').modal('hide');
+          window.location.reload(true);
+      }
+  });
+});
+
+$('body').on('click', '#editCompany', function (event) {
+
+    event.preventDefault();
+    var id = $(this).data('id');
+    console.log(id)
+    $.get('color/' + id + '/edit', function (data) {
+         $('#userCrudModal').html("Edit category");
+         $('#submit').val("Edit category");
+         $('#practice_modal').modal('show');
+         $('#color_id').val(data.data.id);
+         $('#name').val(data.data.name);
+     })
+});
+
+}); 
+</script>
+@endpush 
     
 </x-app-layout>
